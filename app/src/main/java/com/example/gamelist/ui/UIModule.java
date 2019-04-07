131f29dd7221ba6b37c0dd789f4727189320a66b
KalBence <kalman.bence95@gmail.com>
2019-04-07 13:29:03 +0200

    added game interactor and configured Dagger

---
+++ app/src/main/java/com/example/gamelist/ui/UIModule.java
@@ -0,0 +1,21 @@
+package com.example.gamelist.ui;
+
+import android.content.Context;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class UIModule {
+    private Context context;
+
+    public UIModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+
+}
